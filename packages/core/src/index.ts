d91c90398f1b560e621dcdbd84bbc18aabe9e1ea
Sherv Nariman <prosherv@hotmail.com>
2026-07-11 21:14:45 -0400

    export deterministic trace and rule APIs

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,51 @@
 export { defineMotionGuardConfig } from "./config.js";
+export {
+  createFinding,
+  type MotionGuardEvaluation,
+  type MotionGuardEvidenceMetric,
+  type MotionGuardFinding,
+  type MotionGuardFindingConfidence,
+  type MotionGuardFindingEvidence,
+  type MotionGuardFindingSeverity,
+  type MotionGuardRuleId,
+} from "./findings.js";
+export {
+  DEFAULT_MOTIONGUARD_RULES,
+  DEFAULT_RULE_OPTIONS,
+  defineRuleOptions,
+  evaluateMotionGuardTrace,
+  exitedButInteractiveRule,
+  interruptedStateMismatchRule,
+  movingInteractionTargetRule,
+  reducedMotionMismatchRule,
+} from "./rules/index.js";
+export type {
+  MotionGuardRule,
+  MotionGuardRuleContext,
+  MotionGuardRuleOptions,
+} from "./rules/index.js";
+export {
+  DEFAULT_TRACE_LIMITS,
+  MOTIONGUARD_TRACE_SCHEMA_VERSION,
+  defineMotionGuardTrace,
+  distance,
+  findTargetAtOrBefore,
+  isVisuallyHidden,
+  pointInsideRect,
+  rectCenter,
+} from "./trace.js";
+export type {
+  MotionGuardAccessibilitySnapshot,
+  MotionGuardFrame,
+  MotionGuardInteractionEvent,
+  MotionGuardPoint,
+  MotionGuardRect,
+  MotionGuardStateSnapshot,
+  MotionGuardTargetSnapshot,
+  MotionGuardTrace,
+  MotionGuardTraceLimits,
+  MotionGuardVisualSnapshot,
+} from "./trace.js";
 export { MOTIONGUARD_SCHEMA_VERSION } from "./types.js";
 export type {
   MotionGuardConfig,
